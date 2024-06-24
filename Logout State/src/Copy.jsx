fe2d62a5fc7603aff5e12e8d33fcb8173084f452
code-Kartik-Rathore <143636920+code-Kartik-Rathore@users.noreply.github.com>
2024-06-24 22:09:52 +0530

    Add files via upload

---
+++ Logout State/src/Copy.jsx
@@ -0,0 +1,11 @@
+import CardSlider from "./CardSlider";
+import Table from "./Table.jsx";
+
+export default function Copy() {
+  return (
+    <main>
+      <CardSlider />
+      <Table />
+    </main>
+  );
+}
